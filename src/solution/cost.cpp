a2dce796f6846969f1fedfc80efd110878bf1df9
NielsvdL <nielsvanderlaan@hotmail.com>
2017-03-19 21:35:51 +0100

    tuned parameters (far from perfect)

---
+++ src/solution/cost.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Niels on 18-3-2017.
+//
+
+/**
+ * @brief Calculates the total cost of the solution (without penalty).
+ *
+ * @return Total cost of the solution
+ */
+
+#include "solution.ih"
+
+double Solution::cost()
+{
+    double cost = 0;
+    for (Depot &depot: d_depots)
+    {
+        for (Vehicle const &vehicle: depot.getVehicles())
+        {
+            cost += vehicle.getTotalCost();
+        }
+    }
+
+    return cost;
+}
