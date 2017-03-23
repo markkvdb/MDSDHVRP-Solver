37dce5cfbe370148603de1d1c58181fe12fd5100
markkvdb <markkvdb@users.noreply.github.com>
2017-03-23 16:25:22 +0100

    Fixed percentage temperature

--- src/solver/run.cpp
+++ src/solver/run.cpp
@@ -39,7 +39,7 @@ void Solver::run()
         if (d_env->d_newSolution.feasible() && d_env->d_newSolution.totalCost() < d_env->d_bestFeasibleSolution.totalCost())
         {
             d_env->d_bestFeasibleSolution = d_env->d_newSolution;
-//            d_env->d_temp = 0.005 * d_env->d_bestFeasibleSolution.cost();
+            d_env->d_temp = 0.005 * d_env->d_bestFeasibleSolution.cost();
         }
 
         d_env->d_currentSolution = simulatedAnnealing(d_env->d_newSolution, d_env->d_currentSolution);
@@ -77,7 +77,7 @@ void Solver::run()
         if (d_env->d_newSolution.feasible() && d_env->d_newSolution.cost() < d_env->d_bestFeasibleSolution.cost())
         {
             d_env->d_bestFeasibleSolution = d_env->d_newSolution;
-//            d_env->d_temp = 0.005 * d_env->d_bestFeasibleSolution.cost();
+            d_env->d_temp = 0.005 * d_env->d_bestFeasibleSolution.cost();
         }
 
         d_env->d_currentSolution = simulatedAnnealing(d_env->d_newSolution, d_env->d_currentSolution);
